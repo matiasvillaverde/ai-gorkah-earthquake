4c0911ac99c287468c86576f8a342dd6b3710750
mati <matiasvillaverde@protonmail.com>
2023-10-05 15:50:54 +0200

    Refactor main.ipynb and submiter.py
    - Remove `catboost` from the requirements file
    - Add newlines at the end of the file
    - The `model.h5` binary file has changed
    - Add Import Data section to `main.ipynb`
    - Add Impute Data section to `main.ipynb`
    - Add Encode Data section to `main.ipynb`
    - Modify Create a model section in `main.ipynb`
    - Modify Evaluate the model section in `main.ipynb`
    - Modify Submit section in `main.ipynb`
    - Modify `src/__pycache__/submiter.cpython-311.pyc` binary file
    - Comment out line that reads the submission_format.csv file in `src/submiter.py`
    - Comment out line that makes the prediction in `src/submiter.py`
    - Comment out lines that save the prediction to the submission file in `src/submiter.py`

--- src/submiter.py
+++ src/submiter.py
@@ -2,13 +2,13 @@ import pandas as pd
 
 def submit(model, test_values):
     # Load the sample submission file
-    submission = pd.read_csv("../Data/submission_format.csv")
+    # submission = pd.read_csv("../Data/submission_format.csv")
 
-    # Make a prediction
-    predictions = model.predict(test_values)
+    # # Make a prediction
+    # predictions = model.predict(test_values)
 
-    # Save the prediction in the submission file
-    submission.iloc[:, 1:] = predictions
-    submission.to_csv("submission.csv", index=False)
+    # # Save the prediction in the submission file
+    # submission.iloc[:, 1:] = predictions
+    # submission.to_csv("submission.csv", index=False)
 
     print("Submission saved")
