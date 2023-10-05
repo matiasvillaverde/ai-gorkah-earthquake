d6548adf63e16eaec27260dd5873f65829688e88
coztomate <75021486+coztomate@users.noreply.github.com>
2023-10-05 17:30:49 +0200

    Update encoder.py

--- src/encoder.py
+++ src/encoder.py
@@ -4,8 +4,23 @@ import pandas as pd
 def encode(train_values, train_labels, test_values):
 
     # Encode the data
+    train_values = binary_encode(train_values)
+    test_values = binary_encode(test_values)
 
     return (train_values, train_labels, test_values)
+
+def binary_encode(df):
+    '''
+    This function takes a dataframe and returns a binary encoded dataframe
+    '''
+    import category_encoders as ce
+    cols =['land_surface_condition', 'foundation_type', 'roof_type',
+       'ground_floor_type', 'other_floor_type', 'position',
+       'plan_configuration', 'legal_ownership_status', 'geo_level_1_id',
+       'geo_level_2_id', 'geo_level_3_id']
+    encoder = ce.BinaryEncoder(cols=cols)
+    binary_encoded = encoder.fit_transform(df)
+    return binary_encoded
 
 
 def dummy_encode(train_x):
