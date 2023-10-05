dc0d02b0a5f05fe2ff56834b822ceb0b679413ba
mati <matiasvillaverde@protonmail.com>
2023-10-06 00:36:56 +0200

    feat: Refactor data cleaning and model evaluation in main notebook
    - Importing data from a csv file and storing it in a dataframe
    - Cleaning the data by removing categorical rows
    - Encoding the data using an encoder
    - Resampling the train data
    - Creating and evaluating a model using XGBoost
    - Making predictions and saving them in a csv file
    - Refactoring code in `model.py` for clarity and readability

--- src/model.py
+++ src/model.py
@@ -52,20 +52,15 @@ def XGBoost(train_values):
     Create a XGBoost classifier and fit it to the train data.
 
     Args:
-        training data set
+        train_values: training dataset
 
     Returns:
-        ?
+        classifier: trained XGBoost classifier
     """
 
-    data=train_values
-
     # Define the predictor variables (X) and the target variable (y)
-    X = data.drop(['building_id', 'damage_grade'], axis=1)
-    y = data['damage_grade']
-
-    #change categories of y to start from 0 bc softmax likes it that way
-    y = y - 1
+    X = train_values.drop(['building_id', 'damage_grade'], axis=1)
+    y = train_values['damage_grade'] - 1  # change categories of y to start from 0
 
     # Split the dataset into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -77,14 +72,14 @@ def XGBoost(train_values):
         'learning_rate': 0.051232294238614126,
         'subsample': 0.6796645277288101,
         'colsample_bytree': 0.7886065868653529,
-        'objective': 'multi:softmax',  # Specify multi-class classification objective
-        'num_class': 3  # Number of classes in your multi-class problem
+        'objective': 'multi:softmax',
+        'num_class': 3
     }
 
     # Create an XGBoost classifier with the specified hyperparameters
     classifier = xgb.XGBClassifier(**hyperparameters)
 
-    # Fit the classifier to the training data (X_train, y_train)
+    # Fit the classifier to the training data
     classifier.fit(X_train, y_train)
 
     return classifier
